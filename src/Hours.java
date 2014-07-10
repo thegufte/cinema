3136c79dbeb71808f163f4b5f1c11c86e7ead65d
thegufte <gsersur@gmail.com>
2014-07-10 12:31:21 +0300

    Initial commit

---
+++ src/Hours.java
@@ -0,0 +1,4 @@
+
+public enum Hours {
+	TEN_AM,TWELVE ,TWO_PM, FOUR_PM, SIX_PM, EIGHT_PM, TEN_PM;
+}
---
+++ src/Saloon.java
@@ -0,0 +1,12 @@
+
+public class Saloon {
+	private int rows;
+	private int cols;
+	protected String name;
+	
+	public Saloon(String name, int rows, int cols){
+		this.name = name;
+		this.cols = cols;
+		this.rows = rows;
+	}
+}
